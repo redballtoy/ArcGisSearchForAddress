63dd5929ff47e38594161e4f4f5c21be2ce0012b
Dmitry Sorokin <redballtoy@gmail.com>
2020-09-08 15:15:31 +0300

    Fatal signal 11 (SIGSEGV), code 1

--- app/src/main/java/com/gmail/redballtoy/arcgissearchforaddress/MainActivity.java
+++ app/src/main/java/com/gmail/redballtoy/arcgissearchforaddress/MainActivity.java
@@ -5,35 +5,58 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.esri.arcgisruntime.concurrent.ListenableFuture;
+import com.esri.arcgisruntime.loadable.LoadStatus;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.Graphic;
 import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
+import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol;
+import com.esri.arcgisruntime.symbology.TextSymbol;
 import com.esri.arcgisruntime.tasks.geocode.GeocodeParameters;
 import com.esri.arcgisruntime.tasks.geocode.GeocodeResult;
 import com.esri.arcgisruntime.tasks.geocode.LocatorTask;
+import com.esri.arcgisruntime.util.ListenableList;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
+    //new private variables to hold a reference to the search widget, and to other resources
+    private MapView mMapView;
     private SearchView mSearchView = null;
     private GraphicsOverlay mGraphicsOverlay;
     private LocatorTask mLocatorTask = null;
     private GeocodeParameters mGeocodeParameters = null;
+
+    private double lat = //52.52000659999999
+            55.8057;
+    private double lon = //13.404953999999975
+            37.5889;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMapView = findViewById(R.id.mapView);
+        setupMap();
+        setupLocator();
     }
 
+
+    //add code to create the search widget and add it to the action bar
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
@@ -51,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    //code to receive this Intent, verify it is a search action, and call your search method
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
@@ -59,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //perform the geocode search given a search string
     private void queryLocator(final String query) {
         if (query != null && query.length() > 0) {
             mLocatorTask.cancelLoad();
@@ -69,7 +94,16 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void run() {
                     try {
-
+                        List<GeocodeResult> geocodeResults = geocodeFuture.get();
+                        if (geocodeResults.size() > 0) {
+                            displaySearchResult(geocodeResults.get(0));
+                        } else {
+                            Toast.makeText(getApplicationContext(), getString(R.string.nothing_found)
+                                    + " " + query, Toast.LENGTH_LONG).show();
+                        }
+                    } catch (InterruptedException | ExecutionException e) {
+                        Toast.makeText(getApplicationContext(), e.toString(),
+                                Toast.LENGTH_LONG).show();
                     }
                 }
             });
@@ -77,5 +111,77 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    //method that takes a GeocodeResult and displays it on the map
+    private void displaySearchResult(GeocodeResult geocodedLocation) {
+        String displayLabel = geocodedLocation.getLabel();
+        TextSymbol textLabel = new TextSymbol(18, displayLabel,
+                Color.rgb(192, 32, 32),
+                TextSymbol.HorizontalAlignment.CENTER,
+                TextSymbol.VerticalAlignment.BOTTOM);
+        Graphic textGraphic = new Graphic(geocodedLocation.getDisplayLocation(), textLabel);
+        Graphic mapMarker = new Graphic(geocodedLocation.getDisplayLocation(),
+                geocodedLocation.getAttributes(),
+                new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.SQUARE, Color.rgb(255, 0, 0)
+                        , 12.0f));
+        ListenableList allGraphics = mGraphicsOverlay.getGraphics();
+        allGraphics.clear();
+        allGraphics.add(mapMarker);
+        allGraphics.add(textGraphic);
+        mMapView.setViewpointCenterAsync(geocodedLocation.getDisplayLocation());
+    }
+
+    //method for  LocatorTask and GeocodeParameters initialized
+    private void setupLocator() {
+        String locatorService = "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer";
+        mLocatorTask = new LocatorTask(locatorService);
+        mLocatorTask.addDoneLoadingListener(() -> {
+                    if (mLocatorTask.getLoadStatus() == LoadStatus.LOADED) {
+                        mGeocodeParameters = new GeocodeParameters();
+                        mGeocodeParameters.getResultAttributeNames().add("*");
+                        mGeocodeParameters.setMaxResults(1);
+                        mGraphicsOverlay = new GraphicsOverlay();
+                        mMapView.getGraphicsOverlays().add(mGraphicsOverlay);
+                    } else if (mSearchView != null) {
+                        mSearchView.setEnabled(false);
+                    }
+                }
+        );
+        mLocatorTask.loadAsync();
+    }
+
+    private void setupMap() {
+        if (mMapView != null) {
+            Basemap.Type basemapType = Basemap.Type.STREETS_VECTOR;
+            int levelOfDetail = 11;
+            ArcGISMap map = new ArcGISMap(basemapType, lat, lon, levelOfDetail);
+            mMapView.setMap(map);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if (mMapView != null) {
+            mMapView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mMapView != null) {
+            mMapView.resume();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mMapView != null) {
+            mMapView.dispose();
+        }
+        super.onDestroy();
+    }
+
 
 }
